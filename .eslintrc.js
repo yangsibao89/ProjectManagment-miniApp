0ad0359b640ec5d6ff80a0408e331647086e7b15
yangsibao89 <yangsibao89@gmail.com>
2021-03-28 11:58:40 +0800

    page index

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,11 +23,29 @@ module.exports = {
     ecmaVersion: 12
   },
   rules: {
+    // 强制使用一致的缩进
     indent: [
       'error', 2
     ],
+    // 强制使用一致的反勾号、双引号或单引号
     quotes: [
       'error', 'single'
+    ],
+    // 强制使用一致的换行风格
+    'linebreak-style': [
+      'error', 'unix'
+    ],
+    // 要求或禁止使用分号代替 ASI ["always","never"]
+    semi: [
+      'error', 'always'
+    ],
+    // 禁止调用console对象方法,一般在生产环境中使用
+    'no-console': [
+      'off', {
+        allow:
+        ['log', 'warn', 'error', 'info']
+      }
     ]
+
   }
-}
+};
